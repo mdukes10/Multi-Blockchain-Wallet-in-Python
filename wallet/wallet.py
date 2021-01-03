bc9f6c5f9b9ebc87a91a4e061553b8d6d13c6764
mdukes10 <mdukes94@gmail.com>
2021-01-02 21:56:46 -0600

    initial push

---
+++ wallet/wallet.py
@@ -0,0 +1,98 @@
+# import libraries
+import subprocess
+import json
+import os
+from constants import BTC, BTCTEST, ETH
+from pprint import pprint
+from bit import PrivateKeyTestnet
+from bit.network import NetworkAPI
+from web3 import Web3, middleware, Account
+from web3.gas_strategies.time_based import medium_gas_price_strategy
+from web3.middleware import geth_poa_middleware
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# import mnemonic from env
+mnemonic = os.getenv('MNEMONIC')
+
+# connect to local ETH/ geth
+w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
+w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
+
+# Define function to derive wallet
+def derive_wallets(mnemonic, coin, numderive):
+    """Use the subprocess library to call the php file script from Python"""
+    command = f'php ./derive -g --mnemonic="{mnemonic}" --numderive="{numderive}" --coin="{coin}" --format=json' 
+    
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    (output, err) = p.communicate()
+   
+    keys = json.loads(output)
+    return  keys
+
+# Test the function derive_wallets
+derive_wallets(mnemonic, 'BTC', 3)
+
+#Setting dictionary of coins to be used in the wallet
+coins = {"eth", "btc-test", "btc"}
+numderive = 3
+
+keys = {}
+for coin in coins:
+    keys[coin]= derive_wallets(mnemonic, coin, numderive=3)
+    
+eth_PrivateKey = keys["eth"][0]['privkey']
+btc_PrivateKey = keys['btc-test'][0]['privkey']
+
+# create a function that convert the privkey string in a child key to an account object.
+def priv_key_to_account(coin,priv_key):
+    if coin == ETH:
+        return Account.privateKeyToAccount(priv_key)
+    elif coin == BTCTEST:
+        return PrivateKeyTestnet(priv_key)
+
+def create_tx(coin,account, recipient, amount):
+    if coin == ETH: 
+        gasEstimate = w3.eth.estimateGas(
+            {"from":eth_acc.address, "to":recipient, "value": amount}
+        )
+        return { 
+            "from": eth_acc.address,
+            "to": recipient,
+            "value": amount,
+            "gasPrice": w3.eth.gasPrice,
+            "gas": gasEstimate,
+            "nonce": w3.eth.getTransactionCount(eth_acc.address)
+        }
+    
+    elif coin == BTCTEST:
+        return PrivateKeyTestnet.prepare_transaction(account.address,[(recipient, amount, BTC)])
+    
+# create a function to hold Ethereum 
+eth_acc = priv_key_to_account(ETH, derive_wallets(mnemonic, ETH,5)[0]['privkey'])
+
+# create a function to send txn
+def send_txn(coin,account,recipient, amount):
+    txn = create_tx(coin, account, recipient, amount)
+    if coin == ETH:
+        signed_txn = eth_acc.sign_transaction(txn)
+        result = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+        print(result.hex())
+        return result.hex()
+    elif coin == BTCTEST:
+        tx_btctest = create_tx(coin, account, recipient, amount)
+        signed_txn = account.sign_transaction(txn)
+        print(signed_txn)
+        return NetworkAPI.broadcast_tx_testnet(signed_txn)
+    
+# create BTC transaction
+create_tx(BTCTEST,btc_acc,"mnUjQrVXhkCXR1vVeAfKyypycmM54P5gDT", 0.00001)
+
+# send BTC transaction
+send_txn(BTCTEST,btc_acc,"mpx1NJKkr3kgB7EnoyrJazc3p3fCyskFzq", 0.00001)
+
+
+
